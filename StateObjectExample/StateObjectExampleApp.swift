05e7c8d9f5542af5a26ac3995f6ad49768616aa8
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2023-06-28 19:15:00 +0700

    Initial Commit

---
+++ StateObjectExample/StateObjectExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateObjectExampleApp.swift
+//  StateObjectExample
+//
+//  Created by Jason Rich Darmawan Onggo Putra on 28/06/23.
+//
+
+import SwiftUI
+
+@main
+struct StateObjectExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
